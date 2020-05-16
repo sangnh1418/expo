b1b0518ac1990e2b328a9a523b1663ddc0042997
sangnh <sangnh@solazu.com>
2020-05-16 20:19:59 +0700

    confg babel

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,19 @@
-module.exports = function(api) {
-  api.cache(true);
+module.exports = function (api) {
+  api.cache(false)
   return {
     presets: ['babel-preset-expo'],
-  };
-};
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            '~': './src',
+            ASSETS: './assets',
+            CORE: './src/Core',
+            Layouts: './src/Core/Layouts',
+          },
+        },
+      ],
+    ],
+  }
+}
